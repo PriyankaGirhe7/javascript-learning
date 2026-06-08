abd7a696e01e63a77a58b17b94bfbfcaa06869ea
PriyankaGirhe7 <priyankadhawale40@gmail.com>
2026-06-08 11:16:49 +0000

    operation

--- jsbasic/01.js
+++ jsbasic/01.js
@@ -18,7 +18,7 @@ let valueInString = String(score);
 let isLoggedIn = "hello";
 
 let BooleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof isLoggedIn); // "boolean"
+//console.log(typeof isLoggedIn); // "boolean"
 
 //" " = false;
 //"hello" = true;
@@ -29,3 +29,39 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 //console.log(stringNumber); // "33"
 //console.log(typeof stringNumber); // "string"
+
+//*********************Operations******************************* */
+
+
+let value = 3;
+
+let negValue = -value;
+//console.log(negValue); // -3
+
+let str1 = "hello";
+let str2 = "world";
+
+let str3 = str1 + " " + str2;
+//console.log(str3); // "hello world"
+
+//console.log(3 + 4); // 7
+//console.log("3" + "4"); // "34" 
+//console.log("3" + 4); // "34"
+//console.log(3 + 4 + "4");  // "74"
+
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 25 + 10 + 5;
+//console.log(num1, num2, num3); // 40 40 40
+
+let gameCounter = 100;
+
+gameCounter++;
+console.log(gameCounter); // 101
+
+++gameCounter;
+console.log(gameCounter); // 100
