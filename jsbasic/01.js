ed830fa59e8214e0205b7b4367dad25ced019972
PriyankaGirhe7 <priyankadhawale40@gmail.com>
2026-06-08 10:42:57 +0000

    datatypes

---
+++ jsbasic/01.js
@@ -0,0 +1,31 @@
+let score = 33;
+
+//console.log(typeof score);  // "number"
+//console.log(typeof (score)); // "number"
+
+let valueInNumber = Number(score);
+//console.log(valueInNumber); // 33
+//console.log(typeof valueInNumber); // "number"
+
+//"33" = 33
+//"33abc" = NaN
+//true = 1, false = 0;
+
+let valueInString = String(score);
+//console.log(valueInString); // "33"
+//console.log(typeof valueInString); // "string"
+
+let isLoggedIn = "hello";
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof isLoggedIn); // "boolean"
+
+//" " = false;
+//"hello" = true;
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+//console.log(stringNumber); // "33"
+//console.log(typeof stringNumber); // "string"
